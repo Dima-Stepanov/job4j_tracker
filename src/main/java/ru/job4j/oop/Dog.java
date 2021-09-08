55078f445bd384f047251833040b5f9d6c413ddf
dmitryjob4j <haoos@inbox.ru>
2021-09-09 00:06:48 +0300

    1.2.1.Классы и объекты.1.1.Создание объекта[#165452#127053]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+/**
+ * 1.2.1.Классы и объекты
+ * 1.1.Создание объекта[#165452#127053]
+ */
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
