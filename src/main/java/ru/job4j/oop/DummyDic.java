017bef2fe46a4f4ce34f25cf4d513913b8fcac11
dmitryjob4j <haoos@inbox.ru>
2021-09-09 00:35:49 +0300

    1.2.1.Классы и объекты.1.4.Вызов метода с возвращаемым типом.[#167387#127055]

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+/**
+ * 1.2.1.Классы и объекты
+ * 1.4.Вызов метода с возвращаемым типом.[#167387#127055]
+ */
+public class DummyDic {
+    public String engToRus(String eng) {
+        return "Неизвестное слово: " + eng;
+    }
+}
