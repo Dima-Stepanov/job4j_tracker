d38718a5d77e15f050787db349849f3ebf019b3c
dmitryjob4j <haoos@inbox.ru>
2021-09-11 00:23:28 +0300

    1.2.2.Наследование.5.Аннотация@Override[#174891#127061]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -3,8 +3,10 @@ package ru.job4j.inheritance;
 /**
  * 1.2.2.Наследование
  * 4.Переопределение[#174890#127059]
+ * 5.Аннотация@Override[#174891#127061]
  */
 public class JSONReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
         return "{" + System.lineSeparator()
                 + "name : " + name
