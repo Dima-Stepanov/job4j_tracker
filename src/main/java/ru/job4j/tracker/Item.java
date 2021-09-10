00dc33c03c8a529a72b36661d74bab0230872561
dmitryjob4j <haoos@inbox.ru>
2021-09-10 11:15:30 +0300

    1.2.2.Наследование.2.Перегрузка конструктора[#173890#127063]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,26 @@
 package ru.job4j.tracker;
 
+/**
+ * 1.2.2.Наследование
+ * 2.Перегрузка конструктора[#173890#127063]
+ *
+ * @since 10.09.2021
+ */
 public class Item {
     private int id;
     private String name;
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public int getId() {
         return id;
