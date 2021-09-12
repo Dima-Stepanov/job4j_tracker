2e4fa4aa6cc35642205a4ff5de0c19a8aa9c3b81
dmitryjob4j <haoos@inbox.ru>
2021-09-12 09:54:45 +0300

    1.2.3.Инкапсуляция.3.2.Удаление моделей из массива.[#310016]

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -4,19 +4,15 @@ package ru.job4j.pojo;
  * 1.2.3.Инкапсуляция
  * 3.2.Удаление моделей из массива.[#310016]
  *
+ * @version 2
  * @since 11.10.2021
  */
 public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
-        products[index] = null;
-        for (int i = index; i < products.length; i++) {
-            if (i + 1 < products.length && products[i + 1] != null) {
-                products[i] = products[i + 1];
-            } else {
-                products[i] = null;
-                break;
-            }
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
         }
+        products[products.length - 1] = null;
         return products;
     }
 }
