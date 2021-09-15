a51f8fa31385c086b70d5a7f56811717b9599411
dmitryjob4j <haoos@inbox.ru>
2021-09-16 00:36:16 +0300

    1.2.4.Полиморфизм.5.1.Тестирование.Подготовка данных.[#182960#127022]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,5 +1,6 @@
 package ru.job4j.tracker;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,8 +10,9 @@ import static org.hamcrest.Matchers.is;
  * 1.2.4.Полиморфизм
  * 5.Input и полиморфизм.[#181094#127031]test
  * тестируем метод createdItem StartUI
+ * 5.1.Тестирование.Подготовка данных.[#182960#127022]
  *
- * @since 14.10.2021
+ * @since 15.10.2021
  */
 public class StartUITest {
 
@@ -38,5 +40,31 @@ public class StartUITest {
         Item expected1 = new Item("Second");
         assertThat(created0.getName(), is(expected0.getName()));
         assertThat(created1.getName(), is(expected1.getName()));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answer = {
+                String.valueOf(item.getId()),
+                "replace item"
+        };
+        Input input = new StudInput(answer);
+        StartUI.replaceItem(input, tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replace item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("delete ietm");
+        tracker.add(item);
+        int idItem = item.getId();
+        String[] answer = {String.valueOf(idItem)};
+        StartUI.deleteItem(new StudInput(answer), tracker);
+        Assert.assertNull(tracker.findById(idItem));
     }
 }
