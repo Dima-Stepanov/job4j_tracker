458dfa1f2552c10e6cca402ebe252c6ad3062395
dmitryjob4j <haoos@inbox.ru>
2021-09-18 19:26:40 +0300

    1.2.5.Исключения.1.Обеспечить бесперебойную работу приложения Tracker[#789#127037]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -40,7 +40,7 @@ public class StartUI {
             this.showMenu(actions);
             int select = input.askInt("Select : ");
             if (select < 0 || select >= actions.length) {
-                out.println("Wrong input, you can select: 0.. ");
+                out.println("Wrong input, you can select: 0.. " + (actions.length - 1));
                 continue;
             }
             UserAction action = actions[select];
