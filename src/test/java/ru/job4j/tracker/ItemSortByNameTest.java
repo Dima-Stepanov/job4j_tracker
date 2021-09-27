77e43e608e714bc574927bf032e56959106839e8
dmitryjob4j <haoos@inbox.ru>
2021-09-27 17:33:38 +0300

    1.3.4.Сортировка.0.Сортировка[#10096#127203]

--- src/test/java/ru/job4j/tracker/ItemSortByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemSortByNameTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import ru.job4j.collection.ItemSortByNameRevesed;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
