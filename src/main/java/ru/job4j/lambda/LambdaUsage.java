ff1b450e1f4a194c775b5d09650b4d2aafbe81bc
dmitryjob4j <haoos@inbox.ru>
2021-10-06 18:11:51 +0300

    1.4.1.Lambda.2.2.Лямбда блок[#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,24 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+
+/**
+ * 1.4.1.Lambda
+ * 2.2.Лямбда блок[#249208]
+ *
+ * @author Dmitry
+ * @version 1
+ * @since 06.10.2021
+ */
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> cmpText = (left, right) -> {
+            System.out.println("compare - " + left + " : " + right);
+            return left.compareTo(right);
+        };
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.length() + " : " + left.length());
+            return right.length() - left.length();
+        };
+    }
+}
