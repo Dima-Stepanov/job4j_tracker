258fba1fc6e31deeba8457e476b750b89ff29849
dmitryjob4j <haoos@inbox.ru>
2021-10-07 17:44:22 +0300

    1.4.2.Stream API.3.Уникальность элементов и сортировка.[#110225#127092]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,11 +1,14 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * 1.4.2.Stream API.
  * 2.Список адресов[#110062#127093].
+ * 3.Уникальность элементов и сортировка.[#110225#127092]
+ * сортируем по городу, убираем одинаковые адреса.
  *
  * @author Dmitry
  * @version 1
@@ -21,6 +24,8 @@ public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
+                .distinct()
+                .sorted(Comparator.comparing(Address::getCity))
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -2,7 +2,6 @@ package ru.job4j.stream;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,6 +10,7 @@ import static org.hamcrest.Matchers.is;
 /**
  * 1.4.2.Stream API
  * 2.Список адресов[#110062#127093]test
+ * 3.Уникальность элементов и сортировка.[#110225#127092]test
  *
  * @author Dmitry
  * @version 1
@@ -29,6 +29,26 @@ public class ProfilesTest {
         List<Address> expected = List.of(
                 new Address("Krd", "Krasn", 23, 123),
                 new Address("Rostov", "Doloman", 11, 159)
+        );
+        List<Address> rsl = new Profiles().collect(profiles);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenProfilesThenListAddressSortByCitiAndDistinctAdress() {
+        List<Profile> profiles = List.of(
+                new Profile("FIO3",
+                        new Address("Rostov-on-Don", "Doloman", 11, 159)),
+                new Profile("FIO1",
+                        new Address("Krasnodar", "Krasn", 23, 123)),
+                new Profile("FIO2",
+                        new Address("Rostov-on-Don", "Doloman", 11, 159)),
+                new Profile("FIO4",
+                        new Address("Krasnodar", "Krasn", 23, 123))
+        );
+        List<Address> expected = List.of(
+                new Address("Krasnodar", "Krasn", 23, 123),
+                new Address("Rostov-on-Don", "Doloman", 11, 159)
         );
         List<Address> rsl = new Profiles().collect(profiles);
         assertThat(rsl, is(expected));
