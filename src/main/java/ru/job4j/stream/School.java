ddcb5067efffd9d1a314fdf08133f318df7754fa
dmitryjob4j <haoos@inbox.ru>
2021-10-09 00:47:45 +0300

    1.4.2.Stream API.4.Преобразование List в Map.[#110226#127089]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -8,10 +9,12 @@ import java.util.stream.Collectors;
  * 1.4.2.Stream API
  * 1.Фильтрация учеников[#110058#127091]
  * Основной класс
+ * 4.Преобразование List в Map.[#110226#127089]
  *
  * @author Dmitry
  * @version 1
  * @since 07.10.2021
+ * @since 09.10.2021
  */
 public class School {
     /**
@@ -25,5 +28,25 @@ public class School {
         return students.stream()
                 .filter(predict)
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * Преобразование списка Sudent в Map
+     * key Student surname
+     * Value object Student
+     *
+     * @param students List<Student>
+     * @return Map
+     */
+    public Map<String, Student> toMap(List<Student> students) {
+        return students.stream()
+                .collect(Collectors.toMap(
+                        student -> student.getSurname(),
+                        student -> student,
+                        (s1, s2) ->
+                                new Student(
+                                        Math.max(s1.getScore(), s2.getScore()),
+                                        s1.getSurname())
+                ));
     }
 }
--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -2,7 +2,9 @@ package ru.job4j.stream;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 import static org.hamcrest.Matchers.is;
@@ -11,10 +13,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /**
  * 1.4.2.Stream API
  * 1.Фильтрация учеников[#110058#127091]test
+ * 4.Преобразование List в Map.[#110226#127089]test
  *
  * @author Dmitry
  * @version 1
  * @since 07.10.2021
+ * @since 09.10.2021
  */
 public class SchoolTest {
 
@@ -78,6 +82,41 @@ public class SchoolTest {
                 new Student(20, "Surname2"),
                 new Student(30, "Surname3"),
                 new Student(40, "Surname4")
+        );
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenListStudentsThenMapSurnameStudent() {
+        List<Student> students = List.of(
+                new Student(10, "Surname1"),
+                new Student(20, "Surname2"),
+                new Student(30, "Surname3"),
+                new Student(40, "Surname4")
+        );
+        School sc = new School();
+        Map<String, Student> rsl = sc.toMap(students);
+        Map<String, Student> expected = Map.of(
+                "Surname1", new Student(10, "Surname1"),
+                "Surname2", new Student(20, "Surname2"),
+                "Surname3", new Student(30, "Surname3"),
+                "Surname4", new Student(40, "Surname4")
+        );
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenListStudentsThenMapSurnameStudentIsDubleSurname() {
+        List<Student> students = List.of(
+                new Student(10, "Surname1"),
+                new Student(20, "Surname2"),
+                new Student(30, "Surname1")
+        );
+        School sc = new School();
+        Map<String, Student> rsl = sc.toMap(students);
+        Map<String, Student> expected = Map.of(
+                "Surname1", new Student(30, "Surname1"),
+                "Surname2", new Student(20, "Surname2")
         );
         assertThat(rsl, is(expected));
     }
